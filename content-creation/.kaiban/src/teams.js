a0cdd80666b0c64ab5cc3e48365d5d8fb8db6aa7
Joseph Yeoh <yeohzijing.sg@gmail.com>
2025-05-12 15:56:24 +0800

    removing sensitive credentials

---
+++ content-creation/.kaiban/src/teams.js
@@ -0,0 +1,10 @@
+
+    // Auto-generated file
+    import team1 from '/Users/josephyeoh/Playground/kaiban/content-creation/team.kban.js';
+
+    const teams = [
+      team1
+    ];
+
+    export default teams;
+  
